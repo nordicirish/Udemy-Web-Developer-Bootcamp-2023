0534c11d1829094d976d4cd20b430fe1d3a31bdd
Roger Graham <roger.graham@tuni.fi>
2023-05-29 14:27:21 +0300

    Initial commit: Add app.js & index.html

---
+++ Js_Guessing_Game/app.js
@@ -0,0 +1,27 @@
+let max = parseInt(prompt("Enter the maximum number"));
+while (!max) {
+  max = parseInt(prompt("Enter a valid number"));
+}
+
+const targetNum = Math.floor(Math.random() * max) + 1;
+console.log(targetNum);
+
+let guess = prompt("Enter your first guess. Enter q to exit");
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  guess = parseInt(guess);
+  attempts++;
+  if (guess > targetNum) {
+    guess = prompt("Too high, try again");
+  } else {
+    guess = prompt("Too low, try again");
+  }
+}
+
+if (guess === "q") {
+  alert("OK, you quit!");
+} else {
+  alert(`You got it! It took you ${attempts} guesses`);
+}
